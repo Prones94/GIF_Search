6bae68a19f3c76b203a9dd787a590cce406cba3b
Ian Rones <ronesp.85@gmail.com>
2019-09-11 15:00:46 -0700

    Edited template for better viewing, added css file to customize html more

--- app.py
+++ app.py
@@ -7,28 +7,24 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """Return homepage."""
-    # TODO: Extract query term from url
     search = request.args.get('search')
-    # TODO: Make 'params' dict with query term and API key
     payload = {
     	"q": search,
     	"key": "5KADMY4UP11V"
     }
-    # TODO: Make an API call to Tenor using the 'requests' library
     req = requests.get("https://api.tenor.com/v1/search?", params=payload)
     data = req.json()
-    # TODO: Get the first 10 results from the search results
-
     results = data["results"][0:10]
-
-
-    # TODO: Render the 'index.html' template, passing the gifs as a named parameter
-
     return render_template(
         "index.html",
         results=results
         )
 
-
 if __name__ == '__main__':
     app.run(debug=True)
+    
+    # TODO: Extract query term from url
+    # TODO: Render the 'index.html' template, passing the gifs as a named parameter
+    # TODO: Make 'params' dict with query term and API key
+    # TODO: Get the first 10 results from the search results
+    # TODO: Make an API call to Tenor using the 'requests' library
